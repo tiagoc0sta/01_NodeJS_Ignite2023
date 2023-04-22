2164c86ac0947ab787900edbfa7b1169ff3c79f2
Tiago Costa <tiagodpv@gmail.com>
2023-04-21 21:41:08 -0400

    add buffers

--- src/server.js
+++ src/server.js
@@ -36,8 +36,22 @@ Server error responses (500 – 599)*/
 
 const users = [];
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url } = req;
+
+  const buffers = [];
+
+  for await (const chunk of req) {
+    buffers.push(chunk);
+  }
+
+  try {
+    req.body = JSON.parse(Buffer.concat(buffers).toString());
+  } catch {
+    req.body = null;
+  }
+
+  //console.log(body.name);
 
   if (method === 'GET' && url === '/users') {
     return res
@@ -45,10 +59,12 @@ const server = http.createServer((req, res) => {
       .end(JSON.stringify(users));
   }
   if (method === 'POST' && url === '/users') {
+    const { name, email } = req.body;
+
     users.push({
       id: 1,
-      name: 'John Doe',
-      email: 'johndoe@gmail.com',
+      name,
+      email,
     });
 
     return res.writeHead(201).end();
